79aa9ecfeffa5941604af35465ba994a70761651
bspiri <54644338+bspiri@users.noreply.github.com>
2020-04-28 11:49:38 +0300

    Update core.js

--- reduce/core.js
+++ reduce/core.js
@@ -1,17 +1,27 @@
 function sum (array) {
-  // your code here
+  return array.reduce((acc, curr) => acc+curr) 
 };
 
 function productAll (array) {
-  // your code here
+  return array.map (e=> e.reduce((acc, curr)=>acc*curr)).reduce((acc,curr)=> acc*curr);
 };
 
-function objectify (array) {
-  // your code here
-};
+const objectify = matrix => {
+	return matrix.reduce((acc, curr) => {
+		acc[curr[0]] = curr[1];
+		return acc;
+	}, {});
+}
 
 function luckyNumbers (array) {
-  // your code here
+  return array.reduce((str, curr) =>{
+		if(array.indexOf(curr) === array.length-1){
+			str += "and " + curr
+		} else {
+			str += curr + ', '
+		}
+		return str
+	}, 'Your lucky numbers are: ')
 };
 
 module.exports = {

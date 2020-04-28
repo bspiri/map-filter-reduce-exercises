2ac59e6e3fc0b4a23a83574caff2083e1bd42fdb
bspiri <54644338+bspiri@users.noreply.github.com>
2020-04-28 11:53:23 +0300

    Update core.js

--- map/core.js
+++ map/core.js
@@ -6,15 +6,32 @@ function multiplyBy10 (array) {
 }
 
 function shiftRight (array) {
- // your code here
+ return	array.map((item, index)=>{
+		if(index == array.length-1){
+			array.unshift(item)
+			array.pop(item)
+		}
+	} )
+   	console.log(array);
 };
 
 function onlyVowels (array) {
- // your code here
+ let newArray = array.map(word => {
+		for(let i=0; i<word.length; i++){
+			let str = ""
+			if (word[i]=='a' && word[i] =='e' && word[i] =='i' && word[i]!=='o' && word[i]=='u') {
+			word[i].slice(i,1)
+			}
+		}
+	})
+	return newArray
 };
 
 function doubleMatrix (array) {
- // your code here
+ var newArray = array.map(item => {
+    return item.map(index => index*2)
+  })
+  console.log(newArray)
 };
 
 module.exports = {
